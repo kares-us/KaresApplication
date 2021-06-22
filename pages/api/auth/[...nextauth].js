b73e766cff6ffd5c0586e3c898607691f306e344
kares-us <kareshosting@gmail.com>
2021-06-22 15:30:22 -0500

    .

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -5,7 +5,8 @@ export default NextAuth({
     providers: [
         Providers.Google({
             clientId: process.env.GOOGLE_CLIENT_ID,
-            clientSecret: process.env.GOOGLE_CLIENT_SECRET
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+            domain:'https://kares.us/'
         })
     ],
     pages: {
