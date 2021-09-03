74fed754fd16e0413913e7fcb0b0be8e4dc7e606
kares-us <kareshosting@gmail.com>
2021-09-03 01:43:23 -0500

    .

--- components/Homepage/Design/Footer.js
+++ components/Homepage/Design/Footer.js
@@ -4,7 +4,7 @@ export default function Footer() {
     const router = useRouter()
 
     return (
-        <div className='h-16 fixed bottom-0 w-full flex justify-between py-1 px-3 shadow-lg bg-gray-800'>
+        <div className='h-16 fixed bottom-0 w-full flex justify-between py-1 px-3 shadow-lg' style={{ backgroundColor: '#1d4e56' }}>
             <img src={'/img/Cumberlands.png'} className='sm:block hidden h-full w-auto' />
             <p className='text-sm font-medium text-gray-300 my-auto'>© Copyright 2021, Daddy Jack</p>
             <img src={'/img/DaddyJackPart.png'} className='h-full w-auto' onClick={() => router.push('/admin')} />
--- components/Homepage/Design/Header.js
+++ components/Homepage/Design/Header.js
@@ -1,6 +1,6 @@
 export default function Header() {
     return (
-        <div className='h-16 fixed top-0 w-full flex justify-center py-1 px-3 shadow-lg bg-gray-800'>
+        <div className='h-16 fixed top-0 w-full flex justify-center py-1 px-3 shadow-lg' style={{backgroundColor: '#1d4e56'}}>
             <img src='/img/kareslogofull.png' />
         </div>
     )
