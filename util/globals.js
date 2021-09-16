1a3f8397af274b09cfc2e9ff0e3a7b43950cd47c
kares-us <shearertzach@gmail.com>
2021-09-15 22:26:05 -0500

    .

--- util/globals.js
+++ util/globals.js
@@ -1,4 +1,5 @@
-const prodDBURI = 'https://kares-api.herokuapp.com'
+// const prodDBURI = 'https://kares-api.herokuapp.com'
+const prodDBURI = 'https://karesapi.us'
 const localDBURI = 'http://localhost:3001'
 
 export const dbURI = prodDBURI
