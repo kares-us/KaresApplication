5ed8a1b8a345bfa546d1e23886c8eb3233de9019
Zach Shearer <shearertzach@gmail.com>
2022-04-14 13:59:19 -0500

    Added address city & zip

--- components/Homepage/Modals/RegisterForm.js
+++ components/Homepage/Modals/RegisterForm.js
@@ -1,91 +1,203 @@
-import React, { useState } from 'react'
-import { useRouter } from 'next/router'
-import fetchHelper from '../../../util/fetchHelper'
+import React, { useState } from "react";
+import { useRouter } from "next/router";
+import fetchHelper from "../../../util/fetchHelper";
 
-import CountyDropdown from '../../Util/CountyDropdown'
-import Input from '../../Util/Input'
-import QuestionDropDown from '../../Util/QuestionDropdown'
-import Disclaimer from './Disclaimer'
-import Checkbox from '../../Util/Checkbox'
-import Button from '../../Util/Button'
-
+import CountyDropdown from "../../Util/CountyDropdown";
+import Input from "../../Util/Input";
+import QuestionDropDown from "../../Util/QuestionDropdown";
+import Disclaimer from "./Disclaimer";
+import Checkbox from "../../Util/Checkbox";
+import Button from "../../Util/Button";
 
 export default function SimpleForm(props) {
-  const { counties, handleForm, setPageAlert } = props
-  const router = useRouter()
-  const [county, setCounty] = useState(null)
-  const [name, setName] = useState('')
-  const [phone, setPhone] = useState('')
-  const [address, setAddress] = useState('')
-  const [city, setCity] = useState('')
-  const [zipCode, setZipCode] = useState('')
-  const [referral, setReferral] = useState('')
-  const [employmentAssitance, setEmploymentAssitance] = useState('')
-  const [recoveryAssistance, setRecoveryAssistance] = useState('')
-  const [housingAssistance, setHousingAssistance] = useState('')
-  const [foodAssistance, setFoodAssistance] = useState('')
-  const [careerOrJobTraining, setCareerOrJobTraining] = useState('')
-  const [assistanceWithCollege, setAssistanceWithCollege] = useState('')
-  const [incarceratedOrCorrectionalFacility, setIncarceratedOrCorrectionalFacility] = useState('')
-  const [disclaimer, setDisclaimer] = useState(false)
-  const [disclaimerAccepted, setDisclaimerAccepted] = useState(false)
-
-
+  const { counties, handleForm, setPageAlert } = props;
+  const router = useRouter();
+  const [county, setCounty] = useState(null);
+  const [name, setName] = useState("");
+  const [phone, setPhone] = useState("");
+  const [address, setAddress] = useState("");
+  const [city, setCity] = useState("");
+  const [zipCode, setZipCode] = useState("");
+  const [referral, setReferral] = useState("");
+  const [employmentAssitance, setEmploymentAssitance] = useState("");
+  const [recoveryAssistance, setRecoveryAssistance] = useState("");
+  const [housingAssistance, setHousingAssistance] = useState("");
+  const [foodAssistance, setFoodAssistance] = useState("");
+  const [careerOrJobTraining, setCareerOrJobTraining] = useState("");
+  const [assistanceWithCollege, setAssistanceWithCollege] = useState("");
+  const [
+    incarceratedOrCorrectionalFacility,
+    setIncarceratedOrCorrectionalFacility,
+  ] = useState("");
+  const [disclaimer, setDisclaimer] = useState(false);
+  const [disclaimerAccepted, setDisclaimerAccepted] = useState(false);
 
   async function submitRegisterForm(data) {
-    let res = await fetchHelper('/visitor/submit_register', "POST", data)
-    let json = await res.json()
+    let res = await fetchHelper("/visitor/submit_register", "POST", data);
+    let json = await res.json();
 
-    if (!res.ok) setPageAlert({ type: 'Error', message: json.message })
-    else router.push(`/resources/${county._id}`)
-
+    if (!res.ok) setPageAlert({ type: "Error", message: json.message });
+    else router.push(`/resources/${county._id}`);
   }
 
   return (
-    <div className='z-50 fixed top-0 left-0 transform w-full h-full flex justify-center items-center'>
-      <div className='absolute top-0 left-0 z-0 transform w-full h-full bg-black opacity-50' onClick={() => handleForm(null)}></div>
-      <div className='w-11/12 max-w-2xl bg-white z-10 p-6 rounded-md overflow-y-scroll' style={{ maxHeight: '95%' }}>
-        <p className='text-xl font-semibold'>Register for Assistance</p>
-        <hr className='border-2 my-5' />
-        <p className='text-lg font-semibold mb-5'><span className='text-red-600'>Note:</span> This form is for re-entry programs, only. For all other requests please use the "Request Assistance" form.</p>
-        <p className='text-lg font-semibold mb-5'><span className='text-red-600'>Note:</span> All fields on this form are required.</p>
-        <hr className='border-2 my-5' />
-        <p className='text-xl my-2'>County <span className='text-red-500'>*</span></p>
-        <CountyDropdown selected={county ? county.name : null} counties={counties} setCounty={setCounty} />
-        <Input name={'Name'} value={name} handleChange={setName} required />
-        <Input name={'Contact Number'} value={phone} handleChange={setPhone} required />
-        <Input name={'Address'} value={address} handleChange={setAddress} required />
-        <Input name={'City'} value={city} handleChange={setCity} required />
-        <Input name={'Zip Code'} value={zipCode} handleChange={setZipCode} required />
-        <QuestionDropDown question={'Referral Source'} answers={['County Attorney', 'Jail', 'Recovery Center', 'Other']} value={referral} setValue={setReferral} required />
-        <QuestionDropDown question={'Do you need employment assistance?'} answers={['Yes', 'No']} value={employmentAssitance} setValue={setEmploymentAssitance} required />
-        <QuestionDropDown question={'Do you need recovery assistance?'} answers={['Yes', 'No']} value={recoveryAssistance} setValue={setRecoveryAssistance} required />
-        <QuestionDropDown question={'Do you need housing assistance?'} answers={['Yes', 'No']} value={housingAssistance} setValue={setHousingAssistance} required />
-        <QuestionDropDown question={'Do you need food assistance?'} answers={['Yes', 'No']} value={foodAssistance} setValue={setFoodAssistance} required />
-        <QuestionDropDown question={'Do you need career or job training?'} answers={['Yes', 'No']} value={careerOrJobTraining} setValue={setCareerOrJobTraining} required />
-        <QuestionDropDown question={'Do you need assistance with college or technical school?'} answers={['Yes', 'No']} value={assistanceWithCollege} setValue={setAssistanceWithCollege} required />
-        <QuestionDropDown question={'Are you currently incarcerated or in a treatment or correctional facility?'} answers={['Yes', 'No']} value={incarceratedOrCorrectionalFacility} setValue={setIncarceratedOrCorrectionalFacility} required />
+    <div className="z-50 fixed top-0 left-0 transform w-full h-full flex justify-center items-center">
+      <div
+        className="absolute top-0 left-0 z-0 transform w-full h-full bg-black opacity-50"
+        onClick={() => handleForm(null)}
+      ></div>
+      <div
+        className="w-11/12 max-w-2xl bg-white z-10 p-6 rounded-md overflow-y-scroll"
+        style={{ maxHeight: "95%" }}
+      >
+        <p className="text-xl font-semibold">Register for Assistance</p>
+        <hr className="border-2 my-5" />
+        <p className="text-lg font-semibold mb-5">
+          <span className="text-red-600">Note:</span> This form is for re-entry
+          programs, only. For all other requests please use the "Request
+          Assistance" form.
+        </p>
+        <p className="text-lg font-semibold mb-5">
+          <span className="text-red-600">Note:</span> All fields on this form
+          are required.
+        </p>
+        <hr className="border-2 my-5" />
+        <p className="text-xl my-2">
+          County <span className="text-red-500">*</span>
+        </p>
+        <CountyDropdown
+          selected={county ? county.name : null}
+          counties={counties}
+          setCounty={setCounty}
+        />
+        <Input name={"Name"} value={name} handleChange={setName} required />
+        <Input
+          name={"Contact Number"}
+          value={phone}
+          handleChange={setPhone}
+          required
+        />
+        <Input
+          name={"Address"}
+          value={address}
+          handleChange={setAddress}
+          required
+        />
+        <Input name={"City"} value={city} handleChange={setCity} required />
+        <Input
+          name={"Zip Code"}
+          value={zipCode}
+          handleChange={setZipCode}
+          required
+        />
+        <QuestionDropDown
+          question={"Referral Source"}
+          answers={["County Attorney", "Jail", "Recovery Center", "Other"]}
+          value={referral}
+          setValue={setReferral}
+          required
+        />
+        <QuestionDropDown
+          question={"Do you need employment assistance?"}
+          answers={["Yes", "No"]}
+          value={employmentAssitance}
+          setValue={setEmploymentAssitance}
+          required
+        />
+        <QuestionDropDown
+          question={"Do you need recovery assistance?"}
+          answers={["Yes", "No"]}
+          value={recoveryAssistance}
+          setValue={setRecoveryAssistance}
+          required
+        />
+        <QuestionDropDown
+          question={"Do you need housing assistance?"}
+          answers={["Yes", "No"]}
+          value={housingAssistance}
+          setValue={setHousingAssistance}
+          required
+        />
+        <QuestionDropDown
+          question={"Do you need food assistance?"}
+          answers={["Yes", "No"]}
+          value={foodAssistance}
+          setValue={setFoodAssistance}
+          required
+        />
+        <QuestionDropDown
+          question={"Do you need career or job training?"}
+          answers={["Yes", "No"]}
+          value={careerOrJobTraining}
+          setValue={setCareerOrJobTraining}
+          required
+        />
+        <QuestionDropDown
+          question={"Do you need assistance with college or technical school?"}
+          answers={["Yes", "No"]}
+          value={assistanceWithCollege}
+          setValue={setAssistanceWithCollege}
+          required
+        />
+        <QuestionDropDown
+          question={
+            "Are you currently incarcerated or in a treatment or correctional facility?"
+          }
+          answers={["Yes", "No"]}
+          value={incarceratedOrCorrectionalFacility}
+          setValue={setIncarceratedOrCorrectionalFacility}
+          required
+        />
 
-        <div className='flex my-5 justify-center'>
-          <Checkbox checked={disclaimerAccepted} onChange={setDisclaimerAccepted} />
-          <p className='text-center font-bold text-lg ml-3'> I acknowledge and agree to the <span className='text-blue-600 cursor-pointer hover:underline' onClick={() => setDisclaimer(!disclaimer)}>confidentiality agreement.</span></p>
+        <div className="flex my-5 justify-center">
+          <Checkbox
+            checked={disclaimerAccepted}
+            onChange={setDisclaimerAccepted}
+          />
+          <p className="text-center font-bold text-lg ml-3">
+            {" "}
+            I acknowledge and agree to the{" "}
+            <span
+              className="text-blue-600 cursor-pointer hover:underline"
+              onClick={() => setDisclaimer(!disclaimer)}
+            >
+              confidentiality agreement.
+            </span>
+          </p>
         </div>
 
-        <div className='flex justify-evenly items-center mt-5 flex-wrap'>
+        <div className="flex justify-evenly items-center mt-5 flex-wrap">
+          <Button onClick={() => handleForm(null)} color="blue" label="Close" />
           <Button
-            onClick={() => handleForm(null)}
-            color='blue'
-            label='Close'
-          />
-          <Button
-            onClick={() => submitRegisterForm({ county, name, phone, registerInfo: { address, referral, employmentAssitance, recoveryAssistance, housingAssistance, foodAssistance, careerOrJobTraining, assistanceWithCollege, incarceratedOrCorrectionalFacility }, createdAt: Date.now(), requestFulfilled: false })}
-            color='green'
-            label='Submit'
+            onClick={() =>
+              submitRegisterForm({
+                county,
+                name,
+                phone,
+                registerInfo: {
+                  address,
+                  city,
+                  zip,
+                  referral,
+                  employmentAssitance,
+                  recoveryAssistance,
+                  housingAssistance,
+                  foodAssistance,
+                  careerOrJobTraining,
+                  assistanceWithCollege,
+                  incarceratedOrCorrectionalFacility,
+                },
+                createdAt: Date.now(),
+                requestFulfilled: false,
+              })
+            }
+            color="green"
+            label="Submit"
             disabled={!disclaimerAccepted}
           />
         </div>
         {disclaimer ? <Disclaimer handleForm={setDisclaimer} /> : null}
       </div>
     </div>
-  )
+  );
 }
